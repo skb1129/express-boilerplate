827594342341a665cdf89ff7ec485bdca01f857d
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-10-07 13:14:02 +0530

    feat: add controllers and service functions

--- src/apis/product.api.ts
+++ src/apis/product.api.ts
@@ -1,17 +1,54 @@
 import { Router } from "express";
 
 import { ProductService } from "../services";
+import { HTTPStatus } from "../types";
 
 const api = Router();
 
-api.get("/", async (req, res) => {
-  const products = await ProductService.getAll();
-  res.send(products);
+api.get("/", async (req, res, next) => {
+  try {
+    const id = req.query.id as string;
+    const product = await ProductService.get(id);
+    if (!product) res.status(HTTPStatus.NoContent);
+    res.send(product);
+  } catch (e) {
+    next(e);
+  }
+});
+
+api.get("/all/", async (req, res, next) => {
+  try {
+    const products = await ProductService.getAll();
+    res.send(products);
+  } catch (e) {
+    next(e);
+  }
 });
 
 api.post("/", async (req, res, next) => {
   try {
     const product = await ProductService.create(req.body);
+    res.status(HTTPStatus.Created);
+    res.send(product);
+  } catch (e) {
+    next(e);
+  }
+});
+
+api.put("/", async (req, res, next) => {
+  try {
+    const product = await ProductService.update(req.body);
+    res.send(product);
+  } catch (e) {
+    next(e);
+  }
+});
+
+api.delete("/", async (req, res, next) => {
+  try {
+    const id = req.query.id as string;
+    const product = await ProductService.remove(id);
+    res.status(HTTPStatus.Accepted);
     res.send(product);
   } catch (e) {
     next(e);
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,12 +3,30 @@ import { APIError, HTTPStatus } from "../types";
 
 export function create({ name, details }: ProductDocument): Promise<ProductDocument> {
   if (!name || !details)
-    throw new APIError(HTTPStatus.BadRequest, 'Required body parameters "name" or "details" not found.');
+    throw new APIError(HTTPStatus.BadRequest, 'Required parameters "name" or "details" not found.');
   const product = new Product({ name, details });
   return product.save();
 }
 
+export async function get(id: string): Promise<ProductDocument | null> {
+  if (!id) throw new APIError(HTTPStatus.BadRequest, 'Required parameter "id" not found.');
+  return Product.findById(id);
+}
+
 export async function getAll(): Promise<ProductDocument[]> {
-  const products = await Product.find();
-  return products;
+  return Product.find();
+}
+
+export async function update({ id, ...data }: ProductDocument): Promise<ProductDocument> {
+  if (!id) throw new APIError(HTTPStatus.BadRequest, 'Required parameter "id" not found.');
+  const product = await Product.findByIdAndUpdate(id, data, { new: true, upsert: false });
+  if (!product) throw new APIError(HTTPStatus.NotFound, 'The given "id" does not exist in the database');
+  return product;
+}
+
+export async function remove(id: string): Promise<ProductDocument> {
+  if (!id) throw new APIError(HTTPStatus.BadRequest, 'Required parameter "id" not found.');
+  const product = await Product.findByIdAndDelete(id);
+  if (!product) throw new APIError(HTTPStatus.NotFound, 'The given "id" does not exist in the database');
+  return product;
 }
